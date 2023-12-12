26ca85b4d4d40c3388347b01e2bfdf3c50485abb
Cesar <ccordero.007@gmail.cim>
2023-12-11 20:33:23 -0600

    Adding rest fields of user model

--- users/models.py
+++ users/models.py
@@ -15,6 +15,23 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_SPANISH =  "es"
+
+    LANGUAGES_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_SPANISH, "Spanish"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_NIC = "cor"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_NIC, "COR"))
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGES_CHOICES, max_length=2,null=True, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3,null=True, blank=True)
+    superhost = models.BooleanField(default=False)
