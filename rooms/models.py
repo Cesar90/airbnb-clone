c7f2b4a23abd055ee3ecc327b5c18ff4ea143776
Cesar <ccordero.007@gmail.cim>
2023-12-12 22:29:26 -0600

    Configuration the rest of fields for Room model (Facility, Amenity, HouseRule)

--- rooms/models.py
+++ rooms/models.py
@@ -15,9 +15,28 @@ class AbstractItem(core_models.TimeStampedModel):
         return self.name
     
 class RoomType(AbstractItem):
+    """
+        RoomType Model Definition
+    """
     pass
 
+class Amenity(AbstractItem):
+    """
+        Amenity Model Definition
+    """
+    pass
 
+class Facility(AbstractItem):
+    """
+        Facility Model Definition
+    """
+    pass
+
+class HouseRule(AbstractItem):
+    """
+        HouseRule Model Definition
+    """
+    pass
 
 class Room(core_models.TimeStampedModel):
     """
@@ -37,7 +56,12 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instan_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    # room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules = models.ManyToManyField(HouseRule)
+    
 
     def __str__(self) -> str:
         return self.name
