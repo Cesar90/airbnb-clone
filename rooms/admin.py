8bb401b1da4a15c31b3f592ca1188c6d008cc221
Cesar <ccordero.007@gmail.cim>
2023-12-14 07:38:10 -0600

    Room Admin Panel Enhanced

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,6 +13,46 @@ class RoomAdmin(admin.ModelAdmin):
         """
                 Room Admin Definition
         """
+
+        fieldsets = (
+        (
+                "Basic Info",
+                {
+                        "fields": (
+                                "name",
+                                "description",
+                                "country",
+                                "city",
+                                "address",
+                                "price",
+                                "room_type",
+                        )
+                },
+                ),
+                ("Times", {"fields": (
+                                "check_in", 
+                                "check_out", 
+                                "instant_book"
+                                )}),
+                ("Spaces", 
+                        {"fields": (
+                                "guests",
+                                "beds",
+                                "bedrooms",
+                                "baths"
+                        )}),
+                (
+                "More About the Space",
+                        {"fields": 
+                                (
+                                        "amenities",
+                                        "facilities",
+                                        "house_rules"
+                                )},
+                ),
+                ("Last Details", {"fields": ("host",)}),
+        )
+
         list_display = (
                 "name",
                 # "description",
@@ -30,11 +70,18 @@ class RoomAdmin(admin.ModelAdmin):
 
         list_filter = (
                 "instant_book",
+                "host__superhost",
+                "room_type",
+                "amenities",
+                "facilities",
+                "house_rules",
                 "city",
                 "country",
         )
 
         search_fields = ("=city","^host__username")
+
+        filter_horizontal = ("amenities", "facilities", "house_rules")
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
