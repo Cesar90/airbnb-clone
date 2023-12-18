c8d7c3e7692883577d6473973fca032126254b9f
Cesar <ccordero.007@gmail.cim>
2023-12-17 21:59:18 -0600

    Pagination on rooms views

--- rooms/views.py
+++ rooms/views.py
@@ -11,7 +11,14 @@ def all_rooms(request):
     # return HttpResponse(content=f"<h1>{now}</h1>")
     now = datetime.now()
     hungry = True
-    all_rooms = models.Room.objects.all()
+    # print(dir(request.GET.keys()))
+    # print(request.GET.keys())
+    # print(request.GET.get("page", 1))
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={
         "now":now,
         "hungry": hungry,
