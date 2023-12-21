6158d179b8e92633ba310571c336927404acc205
Cesar <ccordero.007@gmail.cim>
2023-12-20 23:36:49 -0600

    Select choices and set values selected

--- rooms/views.py
+++ rooms/views.py
@@ -33,11 +33,24 @@ class RoomDetail(DetailView):
 def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country","NI")
+    room_type = int(request.GET.get("room_type", 0))
     room_types = models.RoomType.objects.all()
-    return render(request, "rooms/search.html", {
+
+    form = {
         "city": city,
-        "countries":countries,
-        "room_types":room_types
+        "s_room_type":room_type,
+        "s_country":country,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types
+    }
+
+    return render(request, "rooms/search.html", {
+        **form,
+        **choices
     })
     
 def room_detail(request, pk):
