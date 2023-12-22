28a3cf652dbef679c2d1150fcaafc2b0275dec50
Cesar <ccordero.007@gmail.cim>
2023-12-21 23:34:25 -0600

    Filter rooms by city, country and room_type

--- rooms/views.py
+++ rooms/views.py
@@ -71,9 +71,26 @@ def search(request):
         "facilities": facilities
     }
 
+    # qs = models.Room.objects.filter()
+    # if price != 0:
+    #     qs = qs.filter(price__lte=price)
+    
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args['room_type__pk__exact'] = room_type
+
+    rooms = models.Room.objects.filter(**filter_args)
+
     return render(request, "rooms/search.html", {
         **form,
-        **choices
+        **choices,
+        "rooms": rooms
     })
     
 def room_detail(request, pk):
