6c51333a5b9fd81b3956605744a8343af98344a1
Cesar <ccordero.007@gmail.cim>
2023-12-28 14:02:47 -0600

    Setting up login view and backend to handle login logic

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request,  "users/login.html")
+
+    def post(self, request):
+        pass
+
+# def login_view(request):
+#     if request.method == "GET":
+#         pass
+#     elif request.method == "POST"
