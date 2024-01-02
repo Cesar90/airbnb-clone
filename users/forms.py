a50c66a00ed0210872ae7971b97b6296951b5778
Cesar <ccordero.007@gmail.cim>
2024-01-02 10:21:01 -0600

    Implement save method in SignUpForm

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from . import models
 
@@ -37,7 +38,52 @@ class LoginForm(forms.Form):
         except models.User.DoesNotExist:
             self.add_error("email",forms.ValidationError("User does not exist"))
 
-class SignUpForm(forms.Form):
+
+class SignUpForm(forms.ModelForm):
+
+    class Meta:
+        model = models.User
+        fields = ("first_name","last_name","email")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.filter(email=email)
+            raise forms.ValidationError("User already exists with email")
+        except models.User.DoesNotExist:
+            return email
+        
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+        
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False) #Create object but not save it
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
+        
+    # def save(self):
+    #     first_name = self.cleaned_data.get("first_name")
+    #     last_name = self.cleaned_data.get("last_name")
+    #     email = self.cleaned_data.get("email")
+    #     password = self.cleaned_data.get("password")
+    #     user = models.User.objects.create_user(email, email, password)
+    #     user.first_name = first_name
+    #     user.last_name = last_name
+    #     user.save()
+
+class SignUpForm_old(forms.Form):
     first_name = forms.CharField(max_length=80)
     last_name = forms.CharField(max_length=80)
     email = forms.EmailField()
